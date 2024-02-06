1fdbfdf703beeda95f0eedf13657da89222bc7a8
Jordon1377 <43587817+Jordon1377@users.noreply.github.com>
2024-02-06 17:06:46 -0500

    Added basic get and post methods that will be expanded on once dafny is implemented.
    Postman set up locally to test requests

--- app.js
+++ app.js
@@ -2,8 +2,40 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const myLogger = function (req, res, next) {
+  console.log("LOGGED");
+  req.requestTime = Date.now();
+  next();
+};
+
+app.use(myLogger);
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  req.body; // JavaScript object containing the parse JSON
+  //res.json(req.body);
+
+  var peopleJSON = JSON.stringify(req.body);
+  res.send(peopleJSON);
+});
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  let responseText = "Hello World!<br>";
+  responseText += `<small>Requested at: ${req.requestTime}</small>`;
+  res.send(responseText);
+});
+
+app.get("/test", (request, response) => {
+  response.contentType("application/json");
+
+  var people = [
+    { name: "Dave", location: "Atlanta" },
+    { name: "Santa Claus", location: "North Pole" },
+    { name: "Man in the Moon", location: "The Moon" },
+  ];
+
+  var peopleJSON = JSON.stringify(people);
+  response.send(peopleJSON);
 });
 
 app.listen(port, () => {
