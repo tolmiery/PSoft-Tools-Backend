94685eb32ff2889b6f4e7d6e229120747fa24baf
Jordon1377 <43587817+Jordon1377@users.noreply.github.com>
2024-02-06 17:30:08 -0500

    Fixed Previous Commit with Typescript and additional Library update

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,50 @@
 // src/index.js
-import express, { Express, Request, Response } from "express";
+import express, {
+  Express,
+  Request,
+  Response,
+  NextFunction,
+  RequestHandler,
+} from "express";
 
 const app: Express = express();
 const port = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("I love this record babe");
+const logRequest: RequestHandler = (req, res, next) => {
+  console.log("LOGGED");
+  next();
+};
+
+app.use(logRequest);
+app.use(express.json());
+
+app.post("/*", (req, res) => {
+  req.body; // JavaScript object containing the parse JSON
+  //res.json(req.body);
+
+  var peopleJSON = JSON.stringify(req.body);
+  res.send(peopleJSON);
+});
+
+app.get("/", (req, res) => {
+  //res.send("Hello World!");
+  let responseText = "Hello World!<br>";
+  res.send(responseText);
+});
+
+app.get("/test", (request, response) => {
+  response.contentType("application/json");
+
+  var people = [
+    { name: "Dave", location: "Atlanta" },
+    { name: "Santa Claus", location: "North Pole" },
+    { name: "Man in the Moon", location: "The Moon" },
+  ];
+
+  var peopleJSON = JSON.stringify(people);
+  response.send(peopleJSON);
 });
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
